05db0d6b58c07d6ae8451fea86cdcdf129f45cd8
Isadora Venkatasawmy <isadora.venkatasawmy@imgarena.com>
2024-01-17 22:39:46 +0000

    re-did CanBalance and CountClumps that I had deleted

--- BasicConcepts/Array3/CanBalance.java
+++ BasicConcepts/Array3/CanBalance.java
@@ -1,11 +1,41 @@
 package BasicConcepts.Array3;
 
-//Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side
-//is equal to the sum of the numbers on the other side.
+// Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side
+// is equal to the sum of the numbers on the other side.
 //
-//        CanBalance([1, 1, 1, 2, 1]) → true
-//        CanBalance([2, 1, 1, 2, 1]) → false
-//        CanBalance([10, 10]) → true
+// CanBalance([1, 1, 1, 2, 1]) → true
+// CanBalance([2, 1, 1, 2, 1]) → false
+// CanBalance([10, 10]) → true
+
+import java.util.Arrays;
 
 public class CanBalance {
+    public static void main(String[] args) {
+        int [] array1 = {1, 1, 1, 2, 1};
+        int [] array2 = {2, 1, 1, 2, 1};
+        int [] array3 = {10, 10};
+        int [] array4 = {2, 1, 2, 3, 1, 1, 1, 4, 7};
+        int [] array5 = {7};
+        System.out.println(canBalanceCheck(array1));
+        System.out.println(canBalanceCheck(array2));
+        System.out.println(canBalanceCheck(array3));
+        System.out.println(canBalanceCheck(array4));
+        System.out.println(canBalanceCheck(array5));
+    }
+
+    public static boolean canBalanceCheck(int [] givenArray) {
+        int sumLeft = 0;
+        Integer sumRight = Arrays.stream(givenArray).sum();
+
+        for (int i = 0; i < givenArray.length-1; i++) {
+            sumLeft += givenArray[i];
+            sumRight -= givenArray[i];
+            if (sumLeft == sumRight) {
+                System.out.println(i);
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
--- BasicConcepts/Array3/CountClumps.java
+++ BasicConcepts/Array3/CountClumps.java
@@ -1,10 +1,39 @@
 package BasicConcepts.Array3;
 
-//Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value. Return the number of clumps in the given array.
+// Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value.
+// Return the number of clumps in the given array.
 //
-//        CountClumps([1, 2, 2, 3, 4, 4]) → 2
-//        CountClumps([1, 1, 2, 1, 1]) → 2
-//        CountClumps([1, 1, 1, 1, 1]) → 1
+// CountClumps([1, 2, 2, 3, 4, 4]) → 2
+// CountClumps([1, 1, 2, 1, 1]) → 2
+// CountClumps([1, 1, 1, 1, 1]) → 1
 
 public class CountClumps {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 2, 3, 4, 4};
+        int[] array2 = {1, 1, 2, 1, 1};
+        int[] array3 = {1, 1, 1, 1, 1};
+        int[] array4 = {1, 2, 2, 3, 4, 4, 7, 7}; // 3
+        int[] array5 = {1, 2, 2, 3, 4, 4, 7, 7, 7, 1}; // 3
+        System.out.println(countClumps(array1));
+        System.out.println(countClumps(array2));
+        System.out.println(countClumps(array3));
+        System.out.println(countClumps(array4));
+        System.out.println(countClumps(array5));
+
+    }
+
+    public static int countClumps(int[] givenArray) {
+        int counter = 0;
+        for (int i = 0; i < givenArray.length-1; i++) {
+            if (givenArray[i] == givenArray[i+1]) {
+                counter ++;
+                int j = i;
+                while (j < givenArray.length-2 && givenArray[j+1] == givenArray[j+2]) {
+                    j++;
+                }
+                i = j+1;
+            }
+        }
+        return counter;
+    }
 }
